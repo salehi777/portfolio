47e997852005f9df99a10aa3697f2cee4ee74d64
Mohammad Salehi <msalehy77@gmail.com>
2025-05-31 20:50:51 +0330

    Feature: Added analytics and register-sw

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import Routes from './routes';
 import MuiThemeProvider from './theme-provider';
-import "keen-slider/keen-slider.min.css";
+import 'keen-slider/keen-slider.min.css';
+import { Analytics } from '@vercel/analytics/react';
+import RegisterSW from './components/register-sw';
 
 export default function App() {
-
   return (
     <>
+      <Analytics />
+      <RegisterSW />
       <MuiThemeProvider>
         <Routes />
       </MuiThemeProvider>
